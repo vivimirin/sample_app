df5fbc31f6df12532ff7e00a5380e3eb07c4ec39
vivimirin <114142522+vivimirin@users.noreply.github.com>
2023-12-31 03:49:55 +0000

    finish static_pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -25,4 +25,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   assert_response :success
   assert_select "title","Contact|Ruby on Rails Tutorial Sample App"
   end
+  test "should get root" do
+  get static_pages_home_url
+  assert_response :success
+  end
 end
